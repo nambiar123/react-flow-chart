e3764344c0b355317660ff4b9bf1a9efd0ddd326
nambiar123 <adithya.kumar532gmail.com>
2023-12-10 17:51:25 +0530

    first commit

---
+++ src/ReactFlowRenderer/elements.js
@@ -0,0 +1,54 @@
+export const nodes = [
+  {
+    id: "1",
+    type: "input",
+    data: {
+      label: "Node 1",
+    },
+    position: { x: 250, y: 0 },
+  },
+  {
+    id: "2",
+    data: {
+      label: "Node 2",
+    },
+    position: { x: 100, y: 100 },
+  },
+  {
+    id: "3",
+    data: {
+      label:  (
+        <div>
+          <p>Node 2</p>
+          <input type="text" placeholder="Enter text" />
+        </div>
+      ),
+
+    },
+    position: { x: 400, y: 100 },
+    style: {
+      background: "#D6D5E6",
+      color: "#333",
+      border: "1px solid #222138",
+      width: 180,
+    },
+  },
+  {
+    id: "4",
+    type: "custom",
+    position: { x: 50, y: 0 },
+    data: {
+      label: (
+        <div>
+          <p>Node 4</p>
+          <input type="file" />
+        </div>
+      ),
+    },
+  }
+];
+
+export const edges = [
+  { id: "e1-2", source: "1", target: "2", type: "smoothstep", animated: true },
+  { id: "e1-3", source: "1", target: "3", type: "smoothstep", animated: true },
+];
